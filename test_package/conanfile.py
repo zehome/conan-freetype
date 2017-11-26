981c4788f7e6ff230d91d0453655733940683095
dimitri <dimi309@users.noreply.github.com>
2017-11-26 23:25:17 +0100

    Applied bincrafters template.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,7 +1,11 @@
-from conans import ConanFile, CMake, RunEnvironment, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
-class DefaultNameConan(ConanFile):
+
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
@@ -12,13 +16,10 @@ class DefaultNameConan(ConanFile):
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
-            fontfile = os.path.join(self.conanfile_directory, 'OpenSans-Bold.ttf')
-            args = '{fontfile} \'lasote!\' > file.txt'.format(fontfile=fontfile)
-            test_binary = os.path.join("bin","test_package")
-
-            if self.settings.os == "Macos":
-                test_binary = "DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), test_binary)
-            elif self.settings.os != "Windows":
-                test_binary = "LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), test_binary)
-
-            self.run("%s %s" % (test_binary, args))
+            bin_path = os.path.join("bin", "test_package")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
