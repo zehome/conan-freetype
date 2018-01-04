45e30275f76e765db9c70e1cd3dd391e242b2832
Jerry W <jerrywiltse@gmail.com>
2018-01-04 12:12:07 -0500

    refresh all

--- conanfile.py
+++ conanfile.py
@@ -4,26 +4,27 @@
 from conans import ConanFile, CMake, tools
 import os
 
+
 class FreetypeConan(ConanFile):
     name = "freetype"
     version = "2.8.1"
-    homepage = "https://www.freetype.org"
     description = "FreeType is a freely available software library to render fonts."
-    folder = "sources"
+    url="http://github.com/bincrafters/conan-freetype"
+    homepage = "https://www.freetype.org"
+    license="BSD"
+    exports = ["LICENSE.md", "FindFreetype.cmake"]
+    exports_sources = ["CMakeLists.txt", "freetype.pc.in"]
+    generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False], "with_harfbuzz": [True, False] }
     default_options = "shared=False", "fPIC=True", "with_harfbuzz=False"
-    generators = "cmake"
-    url="http://github.com/bincrafters/conan-freetype"
-    license="BSD"
-    exports_sources = "CMakeLists.txt", "freetype.pc.in"
-    exports = "FindFreetype.cmake"
-    requires = "libpng/1.6.34@bincrafters/stable", "bzip2/1.0.6@conan/stable"
+    requires = "libpng/[>=1.6.34]@bincrafters/stable", "bzip2/[>=1.0.6]@conan/stable"
     source_url = "http://downloads.sourceforge.net/project/freetype/freetype2/{0}".format(version)
+    source_subfolder = "sources"
 
     def requirements(self):
         if self.options.with_harfbuzz:
-            self.requires.add("harfbuzz/1.7.1@bincrafters/stable")
+            self.requires.add("harfbuzz/[>=1.7.1]@bincrafters/stable")
 
     def config(self):
         del self.settings.compiler.libcxx
@@ -33,9 +34,9 @@ class FreetypeConan(ConanFile):
         source_file = '{0}/{1}'.format(self.source_url, archive_file)      
         tools.get(source_file)
 
-        os.rename('{0}-{1}'.format(self.name, self.version), self.folder)
+        os.rename('{0}-{1}'.format(self.name, self.version), self.source_subfolder)
 
-        tools.replace_in_file(os.path.join('sources','CMakeLists.txt'),
+        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
                               'if (WIN32 AND NOT MINGW AND BUILD_SHARED_LIBS)\n' +
                               '  message(FATAL_ERROR "Building shared libraries on Windows needs MinGW")\n' +
                               'endif ()\n',
@@ -67,24 +68,24 @@ class FreetypeConan(ConanFile):
         cmake.install()
 
     def package(self):
-        self.copy("FindFreetype.cmake", ".", ".")
+        self.copy("FindFreetype.cmake")
 
         # Copy the license files
-        self.copy("%s/docs/FTL*" % self.folder, dst="licenses", ignore_case=True, keep_path=False)
-        self.copy("%s/docs/GPLv2*" % self.folder, dst="licenses", ignore_case=True, keep_path=False)
-        self.copy("%s/docs/LICENSE*" % self.folder, dst="licenses", ignore_case=True, keep_path=False)
+        self.copy("%s/docs/FTL*" % self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
+        self.copy("%s/docs/GPLv2*" % self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
+        self.copy("%s/docs/LICENSE*" % self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
 
         self.copy("freetype.pc", dst=os.path.join('lib','pkgconfig'), ignore_case=True, keep_path=True)
         self.copy("freetype2.pc", dst=os.path.join('lib','pkgconfig'), ignore_case=True, keep_path=True)
 
         #self.copy("*", dst="include", src='include', keep_path=True, symlinks=True)
-        #self.copy(pattern="*.h", dst="include", src="%s/include" % self.folder, keep_path=True)
+        #self.copy(pattern="*.h", dst="include", src="%s/include" % self.source_subfolder, keep_path=True)
         #self.copy("*freetype*.lib", dst="lib", keep_path=False)
         # UNIX
         if not self.options.shared:
             self.copy(pattern="*.a", dst="lib", keep_path=False)
         else:
-            #self.copy(pattern="*.dll*", dst="bin", src="%s/bin" % self.folder, keep_path=False)
+            #self.copy(pattern="*.dll*", dst="bin", src="%s/bin" % self.source_subfolder, keep_path=False)
             self.copy(pattern="*.so*", dst="lib", keep_path=False)
             self.copy(pattern="*.dylib*", dst="lib", keep_path=False)
 
