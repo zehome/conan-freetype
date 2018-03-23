bb988612f650b6eb5fe0db08fa957ddd3e632956
Uilian Ries <uilianries@gmail.com>
2018-03-23 00:59:15 -0300

    Update version to 2.9
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,93 +7,68 @@ import os
 
 class FreetypeConan(ConanFile):
     name = "freetype"
-    version = "2.8.1"
+    version = "2.9"
     description = "FreeType is a freely available software library to render fonts."
-    url="http://github.com/bincrafters/conan-freetype"
+    url = "http://github.com/bincrafters/conan-freetype"
     homepage = "https://www.freetype.org"
-    license="BSD"
+    license = "BSD"
+    author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md", "FindFreetype.cmake"]
     exports_sources = ["CMakeLists.txt", "freetype.pc.in"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False], "with_harfbuzz": [True, False] }
-    default_options = "shared=False", "fPIC=True", "with_harfbuzz=False"
-    requires = "libpng/[>=1.6.34]@bincrafters/stable", "bzip2/[>=1.0.6]@conan/stable"
-    source_url = "http://downloads.sourceforge.net/project/freetype/freetype2/{0}".format(version)
-    source_subfolder = "sources"
+    options = {"shared": [True, False], "fPIC": [True, False], "with_harfbuzz": [True, False], "with_png": [True, False], "with_zlib": [True, False] }
+    default_options = "shared=False", "fPIC=True", "with_harfbuzz=False", "with_png=True", "with_zlib=True"
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
 
     def requirements(self):
+        if self.options.with_png:
+            self.requires.add("libpng/1.6.34@bincrafters/stable")
+        if self.options.with_zlib:
+            self.requires.add("bzip2/1.0.6@conan/stable")
         if self.options.with_harfbuzz:
-            self.requires.add("harfbuzz/[>=1.7.1]@bincrafters/stable")
+            self.requires.add("harfbuzz/1.7.6@bincrafters/stable")
 
-    def config(self):
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
         del self.settings.compiler.libcxx
 
     def source(self):
+        source_url = "http://downloads.sourceforge.net/project/freetype/freetype2/{0}".format(self.version)
         archive_file = '{0}-{1}.tar.gz'.format(self.name, self.version)
-        source_file = '{0}/{1}'.format(self.source_url, archive_file)      
+        source_file = '{0}/{1}'.format(source_url, archive_file)
         tools.get(source_file)
-
         os.rename('{0}-{1}'.format(self.name, self.version), self.source_subfolder)
 
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
-                              'if (WIN32 AND NOT MINGW AND BUILD_SHARED_LIBS)\n' +
-                              '  message(FATAL_ERROR "Building shared libraries on Windows needs MinGW")\n' +
-                              'endif ()\n',
-                              '')
-
-    def fetch_hb(self):
-        return
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["PROJECT_VERSION"] = self.version
+        if self.settings.os == "Windows" and self.options.shared:
+            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+        if self.settings.os != "Windows":
+            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        cmake.definitions["WITH_ZLIB"] = self.options.with_zlib
+        cmake.definitions["WITH_PNG"] = self.options.with_png
+        cmake.definitions["WITH_HarfBuzz"] = self.options.with_harfbuzz
+        cmake.configure(build_dir=self.build_subfolder)
+        return cmake
 
     def build(self):
-        cmake = CMake(self)
-        #cmake.verbose = True
-        cmake.definitions["PROJECT_VERSION"] = self.version
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-
-        if self.settings.os == "Windows" and self.options.shared:
-            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = "On"
-        
-        cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-
-        cmake.definitions["WITH_ZLIB"] = "On"
-        cmake.definitions["WITH_PNG"] = "On"
-        cmake.definitions["WITH_HarfBuzz"] = self.options.with_harfbuzz
-
-        if self.options.with_harfbuzz:
-            self.fetch_hb()
-
-        cmake.configure(build_dir="build")
+        cmake = self.configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
+        cmake = self.configure_cmake()
+        cmake.install()
         self.copy("FindFreetype.cmake")
-
-        # Copy the license files
         self.copy("%s/docs/FTL*" % self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
         self.copy("%s/docs/GPLv2*" % self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
         self.copy("%s/docs/LICENSE*" % self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
 
-        self.copy("freetype.pc", dst=os.path.join('lib','pkgconfig'), ignore_case=True, keep_path=True)
-        self.copy("freetype2.pc", dst=os.path.join('lib','pkgconfig'), ignore_case=True, keep_path=True)
-
-        #self.copy("*", dst="include", src='include', keep_path=True, symlinks=True)
-        #self.copy(pattern="*.h", dst="include", src="%s/include" % self.source_subfolder, keep_path=True)
-        #self.copy("*freetype*.lib", dst="lib", keep_path=False)
-        # UNIX
-        if not self.options.shared:
-            self.copy(pattern="*.a", dst="lib", keep_path=False)
-        else:
-            #self.copy(pattern="*.dll*", dst="bin", src="%s/bin" % self.source_subfolder, keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", keep_path=False)
-            self.copy(pattern="*.dylib*", dst="lib", keep_path=False)
-
     def package_info(self):
-        if self.settings.build_type == "Debug":
-            libname = "freetyped"
-        else:
-            libname = "freetype"
-
-        self.cpp_info.libs = [libname]
+        self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.includedirs.append(os.path.join("include","freetype2"))
